2589d331ed93ed28eddb74a837791f893a609b31
WainaniLee <wlmtexeira@gmail.com>
2016-07-13 22:28:07 -1000

    completed exercise

--- loops.js
+++ loops.js
@@ -1,2 +1,10 @@
 // Do your work here
-//instructions are in the README file
+//instructions are in the README filed
+
+
+var presidents =[ "Washington","Adams","Jefferson","Nadison","Monroe"];
+for (var i = 0; i< presidents.length; i++){
+	console.log(presidents[i]);
+}
+
+//command shift d
